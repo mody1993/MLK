4051ab132fd00624ad11d4ca8d5ef885daf10504
mody1993 <modyalzamzamy4@gmail.com>
2026-06-25 11:13:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ import fetch from 'node-fetch';
 
 const { WOLF } = wolfjs;
 
+// ================== room ==================
+// qsr
+// channelId: 569,
+// targetUserId: 84520028
+// وك
+// channelId:74000,
+// targetUserId:84520029
+// لحن الالعاب 
+// channelId:13219769,
+// targetUserId:76023171
+// لو 
+// channelId:17614046,
+// targetUserId:76023150
 // ================== ACCOUNTS ==================
 
 const ACCOUNTS = [
@@ -13,43 +26,43 @@ const ACCOUNTS = [
 email: process.env.U_MAIL_1,
 password: process.env.U_PASS_1,
 allowedPlayers: ['King'],
-channelId:74000,
-targetUserId:84520029
+channelId: 569,
+targetUserId: 84520028
 },
 {
 email: process.env.U_MAIL_2,
 password: process.env.U_PASS_2,
 allowedPlayers: ['KSA'],
-channelId:74000,
-targetUserId:84520029
+channelId: 569,
+targetUserId: 84520028
 },
 {
 email: process.env.U_MAIL_3,
 password: process.env.U_PASS_3,
 allowedPlayers: ['MKH'],
-channelId:74000,
-targetUserId:84520029
+channelId: 569,
+targetUserId: 84520028
 },
 {
 email: process.env.U_MAIL_4,
 password: process.env.U_PASS_4,
 allowedPlayers: ['SAA'],
-channelId:74000,
-targetUserId:84520029
+channelId: 569,
+targetUserId: 84520028
 },
 {
 email: process.env.U_MAIL_5,
 password: process.env.U_PASS_5,
 allowedPlayers: ['JDH'],
-channelId:74000,
-targetUserId:84520029
+channelId: 569,
+targetUserId: 84520028
 },
 {
 email: process.env.U_MAIL_6,
 password: process.env.U_PASS_6,
 allowedPlayers: ['MLK'],
-channelId:74000,
-targetUserId:84520029
+channelId: 569,
+targetUserId: 84520028
 },
 
 // الحسابات الجديدة
@@ -100,8 +113,8 @@ targetUserId: 84520028
 email: process.env.U_MAIL_13,
 password: process.env.U_PASS_13,
 allowedPlayers: ['Passion'],
-channelId:74000,
-targetUserId:84520029
+channelId: 569,
+targetUserId: 84520028
 }
 ];
 // ================== CONSTANTS ==================
